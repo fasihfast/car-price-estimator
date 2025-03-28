987aa20e60e2b4443266879ee79e1fc39de2ecee
fasihfast <muhammadfasihdj@gmail.com>
2025-03-28 12:36:38 +0500

    increased no.pages from 10 to 15

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ from scraper import get_data
 import pandas as pd
 
 if __name__ == '__main__':
-    data = get_data(num_pages=10)
+    data = get_data(num_pages=15)
     df = pd.DataFrame(data)
     # todo: data cleaning
     df.to_csv('data.csv')
