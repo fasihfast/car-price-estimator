378e8e91da61f5d0573a8940569c09311325b170
Abdullah <mafishere@gmail.com>
2025-03-30 03:00:12 +0500

    changes

--- main.py
+++ main.py
@@ -5,6 +5,114 @@ import joblib
 from sklearn.preprocessing import StandardScaler
 from numpy import expm1
 
+
+def submit_fn(make,model_field,year,min_mileage,max_mileage,fuel_type,engine_capacity,transmission,city):
+    # make,model,year,mileage,fuel_type,engine_capacity,transmission,city,price
+
+    if make:
+        make = make.strip()
+    if model_field:
+        model_field = model_field.strip()
+    # if year:
+    #     input_data['year'] = year
+    if not min_mileage:
+        min_mileage = 0
+    if not max_mileage:
+        max_mileage = 1000000
+    # input_data['mileage'] = (min_mileage + max_mileage) / 2        
+    # if fuel_type != 'Any':
+    #     input_data['fuel_type'] = fuel_type
+    # if engine_capacity:
+    #     input_data['engine_capacity'] = engine_capacity
+    # if trim:
+    #     input_data['trim'] = trim
+    # if transmission != 'Any':
+    #     input_data['transmission'] = transmission
+    if city:
+        city = city.strip()
+        # input_data['city'] = city
+
+    if not make or not model_field or not year or not max_mileage or not city or not fuel_type or not transmission or not engine_capacity:
+        st.error('Please fill in all the fields.')
+        return
+    
+    input_data = {
+        'make': make,
+        'model': model_field,
+        'year': year,
+        'mileage': (min_mileage + max_mileage) / 2,
+        'fuel_type': fuel_type,
+        'engine_capacity': engine_capacity,
+        'transmission': transmission,
+        'city': city
+    }
+
+    df = pd.DataFrame([input_data])
+
+    # cleaning 'mileage' column
+    df['mileage'] = df['mileage'].astype(str).str.replace(r'[a-zA-Z,\s]+', '', regex=True)
+    df['mileage'] = pd.to_numeric(df['mileage'], errors='coerce')
+    df['mileage'] = df['mileage'].astype(int) # ensuring correct data type
+
+    # cleaning 'engine_capacity' column
+    if engine_capacity:
+        df['engine_capacity'] = df['engine_capacity'].astype(str).str.replace(r'[a-zA-Z,\s]+', '', regex=True)
+        df['engine_capacity'] = pd.to_numeric(df['engine_capacity'], errors='coerce')
+        df['engine_capacity'] = df['engine_capacity'].astype(int) # ensuring correct data type
+
+    if year:
+        df['year'] = df['year'].astype(int)
+
+    # scaling
+    numerical_cols = ["year", "mileage", "engine_capacity"]
+    # if year:
+    #     numerical_cols.append("year")
+    # if engine_capacity:
+        # numerical_cols.append("engine_capacity")
+    scaler = StandardScaler()
+    df_original = pd.read_csv('data/data.csv')
+    df[numerical_cols] = scaler.fit(df_original[numerical_cols]).transform(df[numerical_cols]) # important to fit on the original training data
+
+
+    # price conversion
+    def price_convert(value):
+        if value >= 10000000:
+            short_value = round(value / 10000000, 1)
+            return f"{short_value} Crore" 
+    
+        elif value >= 100000:
+            short_value = round(value / 100000, 1)
+            return f"{short_value} Lakh"
+
+    # encoding
+    categorical_cols = ["model", "make", "transmission", "fuel_type", "city"]
+    # if fuel_type != 'Any':
+    #     categorical_cols.append('fuel_type')
+    # if transmission != 'Any':
+    #     categorical_cols.append('transmission')
+    # if make:
+    #     categorical_cols.append('make')
+    # if model_field:
+    #     categorical_cols.append('model')
+    # if city:
+    #     categorical_cols.append('city')
+
+    encoder = joblib.load('data/onehot_encoder.joblib')
+    encoded_features = encoder.transform(df[categorical_cols])
+    encoded_df = pd.DataFrame(encoded_features, columns=encoder.get_feature_names_out(categorical_cols))
+    df = pd.concat([df, encoded_df], axis=1).drop(columns=categorical_cols)
+
+    prediction = model.predict(df)
+
+    if prediction and len(prediction) > 0:
+        price = int(prediction[0])
+        price = expm1(price)
+        new_price=price_convert(price)
+        st.title(f'PKR {new_price}')
+    else:
+        st.title('An error occurred')
+
+
 if __name__ == '__main__':
     model = joblib.load('data/model.joblib')
     
@@ -31,108 +139,23 @@ if __name__ == '__main__':
 
         submit = st.form_submit_button("Estimate Price", use_container_width=True)
 
-    if submit:
-        if make:
-            make = make.trim()
-        if model_field:
-            model_field = model_field.trim()
-        if city: 
-            city = city.trim()
-
-        input_data = {}
-
-        if make:
-            input_data['make'] = make
-        if model_field:
-            input_data['model'] = model_field
-        if year:
-            input_data['year'] = year
-        # if trim:
-        #     input_data['trim'] = trim
-        if not min_mileage:
-            min_mileage = 0
-        if not max_mileage:
-            max_mileage = 1000000
-        input_data['mileage'] = (min_mileage + max_mileage) / 2        
-        if transmission != 'Any':
-            input_data['transmission'] = transmission
-        if engine_capacity:
-            input_data['engine_capacity'] = engine_capacity
-        if fuel_type != 'Any':
-            input_data['fuel_type'] = fuel_type
-        if city:
-            input_data['city'] = city
-
-        df = pd.DataFrame([input_data])
-
-        # cleaning 'mileage' column
-        df['mileage'] = df['mileage'].astype(str).str.replace(r'[a-zA-Z,\s]+', '', regex=True)
-        df['mileage'] = pd.to_numeric(df['mileage'], errors='coerce')
-        df['mileage'] = df['mileage'].astype(int) # ensuring correct data type
-
-        # cleaning 'engine_capacity' column
-        if engine_capacity:
-            df['engine_capacity'] = df['engine_capacity'].astype(str).str.replace(r'[a-zA-Z,\s]+', '', regex=True)
-            df['engine_capacity'] = pd.to_numeric(df['engine_capacity'], errors='coerce')
-            df['engine_capacity'] = df['engine_capacity'].astype(int) # ensuring correct data type
-
-        if year:
-            df['year'] = df['year'].astype(int)
-
-        # scaling
-        numerical_cols = ["mileage"]
-        if year:
-            numerical_cols.append("year")
-        if engine_capacity:
-            numerical_cols.append("engine_capacity")
-        scaler = StandardScaler()
-        df_original = pd.read_csv('data/data.csv')
-        df[numerical_cols] = scaler.fit(df_original[numerical_cols]).transform(df[numerical_cols]) # important to fit on the original training data
-
-
-        # price conversion
-        def price_convert(value):
-            if value >= 10000000:
-                short_value = round(value / 10000000, 1)
-                return f"{short_value} Crore" 
-        
-            elif value >= 100000:
-                short_value = round(value / 100000, 1)
-                return f"{short_value} Lakh"
-
-        # encoding
-        categorical_cols = []
-        if fuel_type != 'Any':
-            categorical_cols.append('fuel_type')
-        if transmission != 'Any':
-            categorical_cols.append('transmission')
-        if make:
-            categorical_cols.append('make')
-        if model_field:
-            categorical_cols.append('model')
-        if city:
-            categorical_cols.append('city')
-        # encoders = joblib.load('data/label_encoders.joblib')
-        # for col in categorical_cols:
-        #     le = encoders[col]
-        #     df[col] = le.transform(df[col])
-
-        encoder = joblib.load('data/onehot_encoder.joblib')
-        encoded_features = encoder.transform(df[categorical_cols])
-        encoded_df = pd.DataFrame(encoded_features, columns=encoder.get_feature_names_out(categorical_cols))
-        df = pd.concat([df, encoded_df], axis=1).drop(columns=categorical_cols)
-
-        prediction = model.predict(df)
-
-        if prediction and len(prediction) > 0:
-            price = int(prediction[0])
-            price = expm1(price)
-            new_price=price_convert(price)
-            st.title(f'PKR {new_price}')
-        else:
-            st.title('An error occurred')
+    if submit: 
+        with st.spinner('Estimating price...'):
+            submit_fn(make,model_field,year,min_mileage,max_mileage,fuel_type,engine_capacity,transmission,city)
 
     st.write('---')
     st.write('-> Created by [mafgit](https://github.com/mafgit) & [fasihfast](https://github.com/fasihfast)')
     st.write('-> Made using machine learning')
     st.write('-> Data source: [PakWheels](https://www.pakwheels.com/)')
+
+# todo: for both min and max mileage, calculate separate estimate and show average. Do the same for year
+# todo: or remove min and max because what if min is set to 0? it will show wrong estimate at 0
+# todo: if city not selected, show average for three most popular cities?
+# todo: if transmission or fuel type not selected, show average of estimates of all possibilities
+# todo: or just remove these extra features?
+# todo: but if we remove them, then only make model and year will remain, and we can just look up csv file to get estimates instead of using a model
+
+# todo: dropdowns for make model etc
+# todo: CI/CD
+# todo: feature engineering
+# todo: gradient booster
